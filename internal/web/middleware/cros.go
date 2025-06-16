0f9d4abfe5fee318f212554a3e806a35ab472340
along <1728717074@qq.com>
2025-06-16 23:01:07 +0800

    refactor: 调整部分跨域设置

--- internal/web/middleware/cros.go
+++ internal/web/middleware/cros.go
@@ -1,29 +1,60 @@
 package middleware
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	"net/http"
+	"time"
 )
 
 func Cors() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-
-		// 处理预检请求
-		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(http.StatusNoContent)
-			return
-		}
-		c.Next()
-	}
-	//return cors.New(cors.Config{
-	//	AllowOrigins:     []string{"*"},
-	//	AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"},
-	//	AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Authorization", "Accept", "X-Requested-With", "X-CSRF-Token"},
-	//	ExposeHeaders:    []string{"Content-Length"},
-	//	AllowCredentials: true,
-	//	MaxAge:           12 * time.Hour,
-	//})
+	//return func(c *gin.Context) {
+	//	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+	//	c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+	//	c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+	//
+	//	// 处理预检请求
+	//	if c.Request.Method == "OPTIONS" {
+	//		c.AbortWithStatus(http.StatusNoContent)
+	//		return
+	//	}
+	//	c.Next()
+	//}
+	return cors.New(cors.Config{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"},
+		AllowHeaders: []string{
+			"Origin",
+			"Content-Type",
+			"Content-Length",
+			"Accept",
+			"Accept-Encoding",
+			"Accept-Language",
+			"Authorization",
+			"X-Requested-With",
+			"X-CSRF-Token",
+			"X-Auth-Token",
+			"Cache-Control",
+			"Connection",
+			"Cookie",
+			"Host",
+			"Pragma",
+			"Referer",
+			"User-Agent",
+		},
+		ExposeHeaders: []string{
+			"Content-Length",
+			"Content-Type",
+			"Cache-Control",
+			"Content-Language",
+			"Content-Disposition",
+			"Access-Control-Allow-Origin",
+			"Authorization",
+		},
+		AllowCredentials: false,
+		MaxAge:           12 * time.Hour,
+		AllowWildcard:    true,
+		// 允许带有通配符的源，如 https://*.example.com
+		AllowPrivateNetwork: true,
+		// 允许从私有网络访问
+	})
 }
